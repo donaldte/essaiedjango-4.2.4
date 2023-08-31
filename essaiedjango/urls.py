da8e9e6842e3f3456333e698c6a4a562f3e21356
donaldte <donaldtedom0@gmail.com>
2023-08-31 13:35:28 +0100

    feat: getting data for db

--- essaiedjango/urls.py
+++ essaiedjango/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view, about_view, contact_view
+from pages.views import home_view, about_view, contact_view, product_detail_view
 
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', home_view, name='home'), # path,url 
     path('about/', about_view, name='about'),
     path('contact/', contact_view, name='contact'),
+    path('product_detail/', product_detail_view, name='product_detail')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from produits.models import Produit
 # Create your views here.
+def product_detail_view(request):
+    obj = Produit.objects.all()
+    context = {
+        'objects': obj 
+    }
+    return render(request, 'produit/detail.html', context)
+
 
 def home_view(request, *args, **kwargs): #args, kwargs
     liste  = ['son', 'maman', 'papa', 'frere', 'soeur']
@@ -18,7 +26,16 @@ def home_view(request, *args, **kwargs): #args, kwargs
 
 def about_view(request, *args, **kwargs): #args, kwargs
     
-    return render(request, "about.html")
+    liste_numbre = [1,2,3,4,5,6,7,8,9,10]
+    
+    context = {
+        'liste_numbre': liste_numbre,
+        'title': 'about us',
+        'my_number': 123,
+        'html': '<center>hello world</center>'
+        }
+    
+    return render(request, "about.html", context)
 
 
 def contact_view(request, *args, **kwargs): #args, kwargs
